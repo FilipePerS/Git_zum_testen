9918a35974b3a0d929bb2f36e7d4ffa0c4fbb2f5
FilipePerS <fpereira@htl-steyr.ac.at>
2020-11-11 09:28:29 +0100

    nu a com

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
         System.out.println("Hallooooooooo Samuuuueeeelllllll!!!!!!111!!1!!");
         System.out.println("Was gehd o??!");
         System.out.println("koiii");
+        System.out.println("this is also here");
     }
 }
