e9e0cc518a385a77f2eeef7be2a964fb9d26436c
FilipePerS <fpereira@htl-steyr.ac.at>
2020-11-11 09:21:17 +0100

    nu a sout gschrim

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hallooooooooo Samuuuueeeelllllll!!!!!!111!!1!!");
+        System.out.println("Was gehd o??!");
     }
 }
