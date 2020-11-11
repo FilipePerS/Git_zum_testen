37e7c013a72a7e0093fde2ecd229f8c8bb32b06c
FilipePerS <fpereira@htl-steyr.ac.at>
2020-11-11 09:26:20 +0100

    koiii

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hallooooooooo Samuuuueeeelllllll!!!!!!111!!1!!");
         System.out.println("Was gehd o??!");
+        System.out.println("koiii");
     }
 }
